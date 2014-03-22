bc12e96191df3a67d48db7f345d9eb7fb591c00d
spezi77 <spezialitz@gmx.de>
2014-03-22 15:21:38 +0100

    Added product make file for htc bravo
    Change-Id: Icd1b290c4e68a1676947c9747040616fff6a92a3

---
+++ products/pa_bravo.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2013 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_bravo,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_hdpi
+
+# Build paprefs from sources
+PREFS_FROM_SOURCE ?= true
+
+# Include ParanoidAndroid common configuration
+include vendor/pa/main.mk
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/htc/bravo/full_bravo.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_bravo
+PRODUCT_BRAND := htc_wwe
+PRODUCT_MODEL := HTC Desire
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_bravo BUILD_FINGERPRINT=htc_wwe/htc_bravo/bravo:2.3.3/GRI40/96875.1:user/release-keys TARGET_BUILD_TYPE=userdebug BUILD_VERSION_TAGS=release-keys PRIVATE_BUILD_DESC="3.14.405.1 CL96875 release-keys"
+
+endif
