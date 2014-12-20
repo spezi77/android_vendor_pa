23ad0547a59945ceaf4b315475d7bf92c00ef51c
spezi77 <spezi7713@gmail.com>
2014-12-20 15:53:11 +0100

    Fix build
    Change-Id: I693d45f4ee0be2b8cf989b5f881d3e88be895f31

--- products/pa_bravo.mk
+++ products/pa_bravo.mk
@@ -29,7 +29,7 @@ $(call inherit-product, vendor/pa/configs/telephony.mk)
 include vendor/pa/main.mk
 
 # Inherit device configuration
-$(call inherit-product, device/htc/bravo/bravo_eu.mk)
+$(call inherit-product, device/htc/bravo/full_bravo.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := pa_bravo
