5c4f6f7e92a807bbaed91908f17fba2c07da1fda
spezi77 <spezi7713@gmail.com>
2015-02-02 20:13:11 +0100

    Cleanup deprecated init.d scripts
    Change-Id: Ia301aca0e920b40bc47bf8eff2f4781d1a6d6f81

--- configs/tools.mk
+++ configs/tools.mk
@@ -23,19 +23,6 @@ PRODUCT_PACKAGES += \
     wget \
     wgetrc
 
-# Utilize init.d scripts
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
-    $(LOCAL_PATH)/etc/init.d/05mountext:system/etc/init.d/05mountext \
-    $(LOCAL_PATH)/etc/init.d/10apps2sd-redux:system/etc/init.d/10apps2sd \
-    $(LOCAL_PATH)/etc/init.d/20extgapps:system/etc/init.d/20extgapps
-
-# Apps2sd files
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/bin/a2sd:system/bin/a2sd \
-    $(LOCAL_PATH)/bin/fix_permissions:system/bin/fix_permissions \
-    $(LOCAL_PATH)/xbin/zipalign:system/xbin/zipalign
-
 # Compcache/Zram support
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bin/compcache:system/bin/compcache \
