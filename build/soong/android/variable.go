f75d3716c0d8664e4db487cf18e5d58c559b196c
spezi77 <spezi7713@gmx.net>
2019-02-11 20:38:19 +0100

    pa: build: Update soong_config for AOSP devices.
    Change-Id: I64edb3de2fadcd91c581dae02d9634a9c254b175
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,10 +1,44 @@
 package android
 type Product_variables struct {
+	Has_legacy_camera_hal1 struct {
+		Cflags []string
+	}
+
+	Uses_media_extensions struct {
+		Cflags []string
+	}
+
+	Cant_reallocate_omx_buffers struct {
+		Cflags []string
+	}
+
+	Qcom_bsp_legacy struct {
+		Cflags []string
+	}
+
+	Qti_flac_decoder struct {
+		Cflags []string
+	}
+
+	Uses_generic_camera_parameter_library struct {
+		Srcs []string
+	}
+
 	Target_shim_libs struct {
 		Cppflags []string
 	}
 }
 
 type ProductVariables struct {
+	Has_legacy_camera_hal1                *bool `json:",omitempty"`
+	Uses_media_extensions                 *bool `json:",omitempty"`
+	Uses_generic_camera_parameter_library *bool `json:",omitempty"`
+	Specific_camera_parameter_library     *string `json:",omitempty"`
+	QTIAudioPath                          *string `json:",omitempty"`
+	QTIDisplayPath                        *string `json:",omitempty"`
+	QTIMediaPath                          *string `json:",omitempty"`
+	Cant_reallocate_omx_buffers           *bool `json:",omitempty"`
+	Qcom_bsp_legacy                       *bool `json:",omitempty"`
+	Qti_flac_decoder                      *bool `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 }
