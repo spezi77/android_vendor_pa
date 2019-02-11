97962b2cce69f995e5ac79b14e065efe8e466030
spezi77 <spezi7713@gmx.net>
2019-02-11 20:53:19 +0100

    Remove some soong variables.
    Change-Id: Idf7bb09485890010eac64e33be7af527882142e4

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,13 +1,5 @@
 package android
 type Product_variables struct {
-	Has_legacy_camera_hal1 struct {
-		Cflags []string
-	}
-
-	Uses_media_extensions struct {
-		Cflags []string
-	}
-
 	Cant_reallocate_omx_buffers struct {
 		Cflags []string
 	}
@@ -20,20 +12,12 @@ type Product_variables struct {
 		Cflags []string
 	}
 
-	Uses_generic_camera_parameter_library struct {
-		Srcs []string
-	}
-
 	Target_shim_libs struct {
 		Cppflags []string
 	}
 }
 
 type ProductVariables struct {
-	Has_legacy_camera_hal1                *bool `json:",omitempty"`
-	Uses_media_extensions                 *bool `json:",omitempty"`
-	Uses_generic_camera_parameter_library *bool `json:",omitempty"`
-	Specific_camera_parameter_library     *string `json:",omitempty"`
 	QTIAudioPath                          *string `json:",omitempty"`
 	QTIDisplayPath                        *string `json:",omitempty"`
 	QTIMediaPath                          *string `json:",omitempty"`
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,15 +1,11 @@
 _contents := $(_contents)    "Aospa":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
-$(call add_json_bool, Has_legacy_camera_hal1,                $(filter true,$(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false)))
-$(call add_json_bool, Uses_media_extensions,                 $(filter true,$(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false)))
-$(call add_json_bool, Uses_generic_camera_parameter_library, $(filter true,$(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true)))
 $(call add_json_bool, BoardUsesQTIHardware,                  $(filter true,$(if $(BOARD_USES_QTI_HARDWARE),true,false)))
 $(call add_json_bool, Cant_reallocate_omx_buffers,           $(filter true,$(if $(filter omap4,$(TARGET_BOARD_PLATFORM)),true,false)))
 $(call add_json_bool, Qcom_bsp_legacy,                       $(filter true,$(if $(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),true,false)))
 $(call add_json_bool, Qti_flac_decoder,                      $(filter true,$(if $(strip $(AUDIO_FEATURE_ENABLED_EXTN_FLAC_DECODER)),true,false)))
 
-$(call add_json_str,  Specific_camera_parameter_library,     $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
 $(call add_json_str,  QTIAudioPath,                          $(call project-path-for,qcom-audio))
 $(call add_json_str,  QTIDisplayPath,                        $(call project-path-for,qcom-display))
 $(call add_json_str,  QTIMediaPath,                          $(call project-path-for,qcom-media))
